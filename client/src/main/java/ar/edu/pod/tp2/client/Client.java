c3539d2c257a0fd1a509d820ff1fef485df12e65
shadad00 <shadad00@itba.edu.ar>
2022-10-22 20:34:01 -0300

    mvn archetype

---
+++ client/src/main/java/ar/edu/pod/tp2/client/Client.java
@@ -0,0 +1,12 @@
+package ar.edu.pod.tp2.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        logger.info("tpe2-g14 Client Starting ...");
+    }
+}
---
+++ server/src/main/java/ar/edu/pod/tp2/server/Server.java
@@ -0,0 +1,12 @@
+package ar.edu.pod.tp2.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("tpe2-g14 Server Starting ...");
+    }
+}
